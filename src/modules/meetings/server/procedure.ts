856a1a6d2e3de5394a5012616392ddc35b04ace4
ign3-0 <njau.wordpress@gmail.com>
2025-07-06 15:51:41 +0300

    Meetings Setup

---
+++ src/modules/meetings/server/procedure.ts
@@ -0,0 +1,86 @@
+import { z } from "zod";
+import { and, count, desc, eq, getTableColumns, ilike } from "drizzle-orm";
+
+import { db } from "@/db";
+import { meetings } from "@/db/schema";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import {
+  Default_Page,
+  Default_Page_Size,
+  Max_Page_Size,
+  Min_Page_Size,
+} from "@/constants";
+
+import { TRPCError } from "@trpc/server";
+
+export const meetingsRouter = createTRPCRouter({
+  getOne: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const [existingMeeting] = await db
+        .select({
+          ...getTableColumns(meetings),
+        })
+        .from(meetings)
+        .where(
+          and(eq(meetings.id, input.id), eq(meetings.userId, ctx.auth.user.id))
+        );
+
+      if (!existingMeeting) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Meeting not found",
+        });
+      }
+
+      return existingMeeting;
+    }),
+  getMany: protectedProcedure
+    .input(
+      z.object({
+        page: z.number().default(Default_Page),
+        pageSize: z
+          .number()
+          .min(Min_Page_Size)
+          .max(Max_Page_Size)
+          .default(Default_Page_Size),
+        search: z.string().nullish(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { search, page, pageSize } = input;
+
+      const data = await db
+        .select({
+          ...getTableColumns(meetings),
+        })
+        .from(meetings)
+        .where(
+          and(
+            eq(meetings.userId, ctx.auth.user.id),
+            search ? ilike(meetings.name, `%${search}%`) : undefined
+          )
+        )
+        .orderBy(desc(meetings.createdAt), desc(meetings.id))
+        .limit(pageSize)
+        .offset((page - 1) * pageSize);
+
+      const [total] = await db
+        .select({ count: count() })
+        .from(meetings)
+        .where(
+          and(
+            eq(meetings.userId, ctx.auth.user.id),
+            search ? ilike(meetings.name, `%${search}%`) : undefined
+          )
+        );
+
+      const totalPages = Math.ceil(total.count / pageSize);
+
+      return {
+        items: data,
+        total: total.count,
+        totalPages,
+      };
+    }),
+});
